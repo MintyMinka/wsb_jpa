c04dfb0dcf8af15c1c2ef984562641835f984f09
Aleksandra Żyłka <261849@student.pwr.edu.pl>
2024-12-08 17:58:41 +0100

    Dodanie przykładowych danych

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -1,2 +1,35 @@
-insert into address (id, address_line1, address_line2, city, postal_code)
-            values (1, 'xx', 'yy', 'city', '62-030');
+-- insert into address (id, address_line1, address_line2, city, postal_code)
+--             values (1, 'xx', 'yy', 'city', '62-030');
+
+-- usuwam istniejące dane (jeśli już jakieś istnieją)
+TRUNCATE TABLE VISIT;
+TRUNCATE TABLE MEDICAL_TREATMENT;
+TRUNCATE TABLE DOCTOR;
+TRUNCATE TABLE PATIENT;
+TRUNCATE TABLE ADDRESS;
+
+-- nowe dane do tabeli Address
+INSERT INTO ADDRESS (id, city, address_line1, address_line2, postal_code) VALUES
+                                                                              (1, 'Gliwice', 'Polna', '9/13', '60-140'),
+                                                                              (2, 'Katowice', 'Brzozowa', '5', '04-190');
+
+-- nowe dane do tabeli Doctor
+INSERT INTO DOCTOR (id, first_name, last_name, telephone_number, email, doctor_number, specialization, address_id) VALUES
+                                                                                                                       (1, 'Jan', 'Kowalski', '123456789', 'janek@example.com', 'DOC123', 'CARDIOLOGY', 1),
+                                                                                                                       (2, 'Karina', 'Mysz', '987654321', 'karina@example.com', 'DOC456', 'PEDIATRICS', 2);
+
+-- nowe dane do tabeli Patient
+INSERT INTO PATIENT (id, first_name, last_name, telephone_number, email, patient_number, date_of_birth, address_id) VALUES
+                                                                                                                        (1, 'Alicja', 'Nowak', '123123123', 'alicja@example.com', 'PAT789', '1985-05-15', 1),
+                                                                                                                        (2, 'Robert', 'Janowski', '321321321', 'bob@example.com', 'PAT101', '1990-08-20', 2);
+
+-- nowe dane do tabeli Visit
+INSERT INTO VISIT (id, description, time, doctor_id, patient_id) VALUES
+                                                                     (1, 'Wizyta kontrolna', '2024-12-08 10:00:00', 1, 1),
+                                                                     (2, 'Obrazowanie przed zabiegiem', '2024-12-08 14:00:00', 2, 2);
+
+-- nowe dane do tabeli MedicalTreatment
+INSERT INTO MEDICAL_TREATMENT (id, description, type, visit_id) VALUES
+                                                                    (1, 'Kontrola funkcji życiowych', 'EKG', 1),
+                                                                    (2, 'Kontrola przed zabiegiem', 'RTG', 2);
+
